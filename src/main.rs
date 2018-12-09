26e3f0e3e6cd8fca395dfdb207d565b8fb650406
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2018-12-09 14:13:16 -0500

    Adding execute command, formatting

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 extern crate rustyline;
 use std::env;
 use std::io;
+use std::process::Command;
 
 fn change_directory(args: &[&str]) -> io::Result<()> {
     let path = args.join(" ");
     env::set_current_dir(path)
 }
 
-fn print_working_directory(args: &[&str]) -> io::Result<()>{
+fn print_working_directory(args: &[&str]) -> io::Result<()> {
     match env::current_dir() {
         Ok(cwd) => {
             println!("Current working dir: {}", cwd.as_path().display());
             Ok(())
-        },
-        Err(err) => {
-            Err(err)
         }
+        Err(err) => Err(err),
     }
 }
 
 fn echo_input(args: &[&str]) -> io::Result<()> {
     println!("{}", args.join(" "));
-    return Ok(())
+    return Ok(());
+}
+
+fn execute_external_commnd(args: &[&str]) -> io::Result<()> {
+    match Command::new(args[0]).args(args[1..].iter()).spawn() {
+        Ok(mut child) => match child.wait() {
+            Ok(stat) => if stat.success() {
+                Ok(())
+            } else {
+                Err(io::Error::new(io::ErrorKind::Other, "Command failed!"))
+            },
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
 }
 
 fn parse_and_execute(line: &str) {
@@ -31,16 +44,17 @@ fn parse_and_execute(line: &str) {
             Err(err) => println!("{:?}", err),
             _ => (),
         },
-        "pwd" => match print_working_directory(&v[1..]){
+        "pwd" => match print_working_directory(&v[1..]) {
             Err(err) => println!("{:?}", err),
             _ => (),
-        }
-        "echo" => {
-            match echo_input(&v[1..]) {
-                _ => ()
-            }
-        }
-        _ => println!("Invalid command!"),
+        },
+        "echo" => match echo_input(&v[1..]) {
+            _ => (),
+        },
+        _ => match execute_external_commnd(&v) {
+            Err(err) => println!("{:?}", err),
+            _ => (),
+        },
     }
 }
 
